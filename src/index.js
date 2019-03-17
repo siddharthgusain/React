535dcd52f24c463016b621f917e5dc64845db408
siddharthgusain <siddharthgusain26011999@gmail.com>
2019-03-18 01:32:32 +0530

    "deleted"

--- src/index.js
+++ src/index.js
@@ -19,34 +19,6 @@ const App = ()=>{
                 <div className="text">Nice Blog post</div>
                 </div>    
             </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() }/>
-                </a>
-                <div className="content">
-                <a href="/" className="author">
-                    SID
-                </a>
-                <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
-                </div>
-                <div className="text">Nice Blog post</div>
-                </div>    
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() }/>
-                </a>
-                <div className="content">
-                <a href="/" className="author">
-                    SID
-                </a>
-                <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
-                </div>
-                <div className="text">Nice Blog post</div>
-                </div>    
-            </div>
         </div>
     );
 };
