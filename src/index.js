2af9677119d263ab0b8c1db496d344a9668c6f34
siddharthgusain <siddharthgusain26011999@gmail.com>
2019-03-19 02:07:03 +0530

    "using props.children"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
+import CommentDetail from './components/CommentDetail/CommentDetail';
+import ApprovalCard from './components/ApprovedCard/ApprovalCard';
 const App = ()=>{
     const name = ['sid','Thanos','Gamora'];
     const time = ['Today at 6:00P.M','Today at 7:00P.M','Today at 8:00P.M'];
@@ -9,21 +9,25 @@ const App = ()=>{
     return (
         <div className="ui container comments">
            <ApprovalCard>
-               <CommentDetail 
-                    name={name[0]}
-                    time={time[0]} 
-                    text={text[0]}/>
+            <CommentDetail 
+                name={name[0]}
+                time={time[0]} 
+                text={text[0]}/>
           </ApprovalCard>
-          
-           <CommentDetail  
+
+          <ApprovalCard>
+            <CommentDetail  
                 name={name[1]} 
                 time={time[1]} 
                 text={text[1]}/>
+          </ApprovalCard>
+
+          <ApprovalCard>
            <CommentDetail 
                 name={name[2]}
                 time={time[2]} 
                 text={text[2]}/>
-           
+            </ApprovalCard>
         </div>
     );
 };
